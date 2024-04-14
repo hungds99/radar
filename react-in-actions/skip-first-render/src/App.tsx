8493f034f7e1068efed8dfd7f24143e6609ea791
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-04-14 09:41:13 +0700

    solve my first time render component

--- react-in-actions/skip-first-render/src/App.tsx
+++ react-in-actions/skip-first-render/src/App.tsx
@@ -1,23 +1,34 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 export default function App() {
   const isFirstRender = useRef(true);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     if (isFirstRender.current) {
       return;
     }
-
-    console.info('=> useEffect: First render');
-  }, []);
+    console.info('useEffect render', count);
+  }, [count]);
 
   return (
     <div>
       <h2>
         React Render Optimize: Skip first <em>useEffect</em> render !!!
       </h2>
-      <button>Click me</button>
+      <br />
+      <div>
+        <p>Count: {count}</p>
+        <button
+          onClick={() => {
+            isFirstRender.current && (isFirstRender.current = false);
+            setCount(count + 1);
+          }}
+        >
+          Click me
+        </button>
+      </div>
     </div>
   );
 }

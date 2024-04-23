4c6bf74aa3731e0a60684aeabdc53df2694e578f
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-04-23 16:57:34 +0700

    lib: add heic convert

--- libs/heic-convert.js
+++ libs/heic-convert.js
@@ -1,13 +1,14 @@
 const { promisify } = require('util');
 const fs = require('fs');
 const convert = require('heic-convert');
+const sharp = require('sharp');
 
 (async () => {
   const outputBuffer = await convert({
-    buffer: fs.readFileSync('./assets/qaungac.heic'), // the HEIC file buffer
-    format: 'JPEG',      // output format
-    quality: 1           // the jpeg compression quality, between 0 and 1
+    buffer: fs.readFileSync('./assets/sample1.heic'),
+    format: 'JPEG',
+    quality: 1,
   });
-
-  await promisify(fs.writeFile)('./assets/qaungac.jpg', outputBuffer);
+  // await promisify(fs.writeFile)('./assets/qaungac.jpg', outputBuffer);
+  await sharp(outputBuffer).rotate(180).toFile('./assets/sample1.jpg');
 })();
